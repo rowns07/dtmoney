ff41b39ec763ed01d7b4ce64abb400dcccaa6a22
Diego Souza <diegosouza@tradecom.com.br>
2022-06-14 19:40:21 -0300

    Adicionando soma de valores

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -13,29 +13,29 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-  const {createTransaction} = useContext(TransactionsContext);
+  const { createTransaction } = useContext(TransactionsContext);
 
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       type,
       amount,
       title,
       category,
     })
-    // const data = {
-    //   title,
-    //   value,
-    //   category,
-    //   type,
-    // }
-    // api.post('/transactions', data)
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
+
   }
 
   return (
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,35 +1,77 @@
+import { useContext } from 'react';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
 import { Container } from "./styles";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const totalDeposits = transactions.reduce((accumulator, transaction) => {
+    if (transaction.type === 'deposit') {
+      return accumulator + transaction.amount
+    }
+    return accumulator;
+  }, 0);
+
+  const summary = transactions.reduce((accumulator, transaction) => {
+
+    if (transaction.type === 'deposit') {
+      accumulator.deposits += transaction.amount;
+      accumulator.total += transaction.amount;
+    } else {
+      accumulator.withdraws += transaction.amount;
+      accumulator.total += transaction.amount;
+    }
+
+    return accumulator
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
 
   return (
-      <Container>
-        <div>
-          <header>
-            <p>Entradas</p>
-            <img src={incomeImg} alt="Entradas" />
-          </header>
-          <strong>R$1000</strong>
-        </div>
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
+      </div>
 
-        <div>
-          <header>
-            <p>Saidas</p>
-            <img src={outcomeImg} alt="Entradas" />
-          </header>
-          <strong>R$ 500,00</strong>
-        </div>
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={outcomeImg} alt="Entradas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)}
+        </strong>
+      </div>
 
-        <div className="highlight-background">
-          <header>
-            <p>Total</p>
-            <img src={totalImg} alt="Entradas" />
-          </header>
-          <strong>R$500,00</strong>
-        </div>
-      </Container>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Entradas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
+      </div>
+    </Container>
   )
 }
